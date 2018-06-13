f6f73137efd24e2d36bc0a8dcfa4087bc084ac22
teeaaspoon <tommypoon36@gmail.com>
2018-06-13 17:17:24 +0000

    implemented display username from cookie

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // use ejs as templating engine
 app.set("view engine", "ejs");
@@ -25,25 +27,34 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-    let templateVars = { urls: urlDatabase };
+    let templateVars = {
+        urls: urlDatabase,
+        username: req.cookies["username"]
+    };
     res.render("urls_index", templateVars);
 });
 
 // renders page for new url to shorten
 app.get("/urls/new", (req, res) => {
-    res.render("urls_new");
+    let templateVars = {
+        urls: urlDatabase,
+        username: req.cookies["username"]
+    };
+    res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
     let templateVars = {
         shortURL: req.params.id,
-        urls: urlDatabase
+        urls: urlDatabase,
+        username: req.cookies["username"]
     };
     res.render("urls_show", templateVars);
 });
 
 // waits for a post to /urls and redirects them to the page where they can look at the short and long url
 app.post("/urls", (req, res) => {
+    //CHECK IF REQ.BODY IS EMPTY STRING IF IT IS ADD ERROR
     // generates a random 6 alpha numeric string
     var shortURL = generateRandomString();
 
@@ -81,6 +92,12 @@ app.get("/u/:shortURL", (req, res) => {
         // response redirect to the long url
         res.redirect(longURL);
     }
+});
+
+// route to login
+app.post("/login", (req, res) => {
+    res.cookie("username", req.body["username"]);
+    res.redirect("/urls");
 });
 
 app.listen(PORT, function() {
